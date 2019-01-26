00aabe133015fe0d058907d435ca04bdbb7409e1
Alex Cooper <cooperalex@live.co.uk>
2019-01-26 17:27:36 -0500

    Take the user straight to the feed if they're already logged in

--- app/src/main/java/com/coopra/nebulus/WelcomeActivity.java
+++ app/src/main/java/com/coopra/nebulus/WelcomeActivity.java
@@ -1,14 +1,18 @@
 package com.coopra.nebulus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 
 public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkForExistingToken();
+
         setContentView(R.layout.activity_welcome);
 
         Fragment welcomeFragment = new WelcomeFragment();
@@ -18,5 +22,13 @@ public class WelcomeActivity extends AppCompatActivity {
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         ft.addToBackStack(null);
         ft.commit();
+    }
+
+    private void checkForExistingToken() {
+        if (!TextUtils.isEmpty(TokenHandler.getToken(this))) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            finish();
+            startActivity(intent);
+        }
     }
 }
