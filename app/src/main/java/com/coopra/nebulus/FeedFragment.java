8c30cc3b028f733edefa71072f77b7798167d57d
Alex Cooper <cooperalex@live.co.uk>
2019-02-05 21:18:35 -0500

    Implement swipe to refresh

--- app/src/main/java/com/coopra/nebulus/FeedFragment.java
+++ app/src/main/java/com/coopra/nebulus/FeedFragment.java
@@ -3,12 +3,16 @@ package com.coopra.nebulus;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.paging.PagedList;
+
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,14 +20,26 @@ import android.view.ViewGroup;
 import com.coopra.database.entities.Track;
 import com.coopra.nebulus.view_models.FeedViewModel;
 
-public class FeedFragment extends Fragment {
+public class FeedFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
     private TrackListAdapter mAdapter;
+    private FeedViewModel mViewModel;
+    private SwipeRefreshLayout mSwipeRefreshView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FeedViewModel feedViewModel = ViewModelProviders.of(this).get(FeedViewModel.class);
-        feedViewModel.getAllTracks().observe(this, new Observer<PagedList<Track>>() {
+        mViewModel = ViewModelProviders.of(this).get(FeedViewModel.class);
+
+        mViewModel.getNetworkState().observe(this, new Observer<FeedViewModel.NetworkStates>() {
+            @Override
+            public void onChanged(FeedViewModel.NetworkStates networkState) {
+                if (mSwipeRefreshView != null) {
+                    mSwipeRefreshView.setRefreshing(networkState == FeedViewModel.NetworkStates.LOADING);
+                }
+            }
+        });
+
+        mViewModel.getAllTracks().observe(this, new Observer<PagedList<Track>>() {
             @Override
             public void onChanged(@Nullable PagedList<Track> tracks) {
                 mAdapter.submitList(tracks);
@@ -45,5 +61,13 @@ public class FeedFragment extends Fragment {
         mAdapter = new TrackListAdapter(getContext());
         recyclerView.setAdapter(mAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mSwipeRefreshView = view.findViewById(R.id.swipe_refresh_view);
+        mSwipeRefreshView.setOnRefreshListener(this);
+    }
+
+    @Override
+    public void onRefresh() {
+        mViewModel.refreshFeed();
     }
 }
--- app/src/main/java/com/coopra/nebulus/view_models/FeedViewModel.java
+++ app/src/main/java/com/coopra/nebulus/view_models/FeedViewModel.java
@@ -1,23 +1,88 @@
 package com.coopra.nebulus.view_models;
 
 import android.app.Application;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.paging.PagedList;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+import com.coopra.data.DashboardActivity;
+import com.coopra.data.DashboardActivityEnvelope;
 import com.coopra.database.entities.Track;
+import com.coopra.database.entities.User;
+import com.coopra.nebulus.TokenHandler;
 import com.coopra.nebulus.TrackRepository;
+import com.coopra.service.service_implementations.ActivitiesService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FeedViewModel extends AndroidViewModel {
     private LiveData<PagedList<Track>> mAllTracks;
+    private MutableLiveData<NetworkStates> mNetworkState;
+    private TrackRepository mRepository;
 
     public FeedViewModel(Application application) {
         super(application);
-        TrackRepository repository = new TrackRepository(application);
-        mAllTracks = repository.getAll();
+        mRepository = new TrackRepository(application);
+        mAllTracks = mRepository.getAll();
     }
 
     public LiveData<PagedList<Track>> getAllTracks() {
         return mAllTracks;
+    }
+
+    public MutableLiveData<NetworkStates> getNetworkState() {
+        if (mNetworkState == null) {
+            mNetworkState = new MutableLiveData<>();
+        }
+
+        return mNetworkState;
+    }
+
+    public void refreshFeed() {
+        getNetworkState().postValue(NetworkStates.LOADING);
+
+        ActivitiesService.getFeedTracks(TokenHandler.getToken(getApplication()), new Callback<DashboardActivityEnvelope>() {
+            @Override
+            public void onResponse(@NonNull Call<DashboardActivityEnvelope> call, @NonNull Response<DashboardActivityEnvelope> response) {
+                handleSuccessfulNetworkCall(response);
+                getNetworkState().postValue(NetworkStates.NORMAL);
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<DashboardActivityEnvelope> call, @NonNull Throwable t) {
+                getNetworkState().postValue(NetworkStates.NORMAL);
+            }
+        });
+    }
+
+    private void handleSuccessfulNetworkCall(@NonNull Response<DashboardActivityEnvelope> response) {
+        if (response.body() != null) {
+            List<Track> tracks = new ArrayList<>();
+            List<User> users = new ArrayList<>();
+
+            for (DashboardActivity activity : response.body().collection) {
+                if (activity.origin != null) {
+                    tracks.add(new Track(activity.origin, response.body().next_href, activity.created_at));
+                    users.add(new User(activity.origin.user));
+                }
+            }
+
+            if (tracks.size() > 0) {
+                mRepository.insertAll(new TrackRepository.TrackParameters(tracks, users));
+            }
+        }
+    }
+
+    public enum NetworkStates {
+        LOADING,
+
+        NORMAL
     }
 }
