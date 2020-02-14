1bfc57a018b17916a87e5eac34746e14dd6f5c6a
Alex Cooper <alcoope@microsoft.com>
2020-02-13 19:23:10 -0800

    Convert ActivitiesService to Kotlin

---
+++ service/src/main/java/com/coopra/service/interfaces/ActivitiesInterface.kt
@@ -0,0 +1,15 @@
+package com.coopra.service.interfaces
+
+import com.coopra.data.DashboardActivityEnvelope
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Url
+
+interface ActivitiesInterface {
+    @GET("me/activities/tracks.json?limit=50")
+    fun getFeedTracks(@Query("oauth_token") token: String): Call<DashboardActivityEnvelope>
+
+    @GET
+    fun getNextTracks(@Url url: String, @Query("oauth_token") token: String): Call<DashboardActivityEnvelope>
+}
